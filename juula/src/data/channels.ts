10c8f1258a5b4cc4315051d1ad1e8f8b8fab736f
zenxha <komays5042@gmail.com>
2025-01-27 15:43:30 -0800

    id attribute for channels

--- juula/src/data/channels.ts
+++ juula/src/data/channels.ts
@@ -1,128 +1,121 @@
 export const channels = [
     {
-      name: "NHK_G",
+      id: "nhk_g",
       group: "Tokyo",
       url: "http://vthanh.utako.moe/NHK_G/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/6/6f/NHK%E7%B7%8F%E5%90%88%E3%83%AD%E3%82%B42020-.png",
+      name: "NHK G"
     },
     {
-      name: "NHK_E",
+      id: "nhk_e",
       group: "Tokyo",
       url: "http://vthanh.utako.moe/NHK_E/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/a/aa/NHKE%E3%83%86%E3%83%AC%E3%83%AD%E3%82%B42020-.png",
+      name: "NHK E"
     },
     {
-      name: "NTV",
+      id: "ntv",
       group: "Tokyo",
       url: "http://vthanh.utako.moe/Nippon_TV/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Nippon_TV_logo_2014.svg/2560px-Nippon_TV_logo_2014.svg.png",
+      name: "NTV"
     },
     {
-      name: "TBS",
+      id: "tbs",
       group: "Tokyo",
       url: "Http://vthanh.utako.moe/TBS/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Tokyo_Broadcasting_System_logo_2020.svg/2560px-Tokyo_Broadcasting_System_logo_2020.svg.png",
+      name: "TBS"
     },
     {
-      name: "Fuji_TV",
+      id: "fuji_tv",
       group: "Tokyo",
       url: "http://vthanh.utako.moe/Fuji_TV/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Fuji_TV_Logo.svg/1049px-Fuji_TV_Logo.svg.png",
+      name: "Fuji TV"
     },
     {
-      name: "TV_Asahi",
+      id: "tv_asahi",
       group: "Tokyo",
       url: "http://vthanh.utako.moe/TV_Asahi/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/TV_Asahi_Logo.svg/2560px-TV_Asahi_Logo.svg.png",
+      name: "TV Asahi"
     },
     {
-      name: "TV_Tokyo",
+      id: "tv_tokyo",
       group: "Tokyo",
       url: "http://vthanh.utako.moe/TV_Tokyo/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/TV_Tokyo_logo_2023.svg/2560px-TV_Tokyo_logo_2023.svg.png",
+      name: "TV Tokyo"
     },
     {
-      name: "TOKYO_MX1",
+      id: "tokyo_mx1",
       group: "Tokyo",
       url: "http://vthanh.utako.moe/Tokyo_MX1/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Tokyo_metropolitan_television_logo_%28rainbow%29.svg/2560px-Tokyo_metropolitan_television_logo_%28rainbow%29.svg.png",
+      name: "TOKYO MX1"
     },
     {
-      name: "NHK_BS",
+      id: "nhk_bs",
       group: "BS",
       url: "http://vthanh.utako.moe/NHK_BS/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/6/6c/NHK_BS.png",
+      name: "NHK BS"
     },
     {
-      name: "Animax",
+      id: "animax",
       group: "BS",
       url: "http://vthanh.utako.moe/Animax/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Animax.svg/1920px-Animax.svg.png",
+      name: "Animax"
     },
     {
-      name: "BS11",
+      id: "bs11",
       group: "BS",
       url: "http://vthanh.utako.moe/BS11/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/BS11_logo.svg/2560px-BS11_logo.svg.png",
+      name: "BS11"
     },
     {
-      name: "NHK_BS4K-test",
+      id: "nhk_bs4k_test",
       group: "BS2",
       url: "http://luong.utako.moe/bsp4k_testsignal/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/NHK_BSP4K_2023_logo.svg/1920px-NHK_BSP4K_2023_logo.svg.png",
+      name: "NHK BS4K"
     },
     {
-      name: "BS_NTV-test",
-      group: "BS2",
-      url: "http://luong.utako.moe/bsntva11/index.m3u8",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/BS4_logo.svg/2560px-BS4_logo.svg.png",
-    },
-    {
-      name: "JSport_1-test",
+      id: "jsport_1_test",
       group: "BS2",
       url: "http://luong.utako.moe/js1/index.m3u8",
       logo: "https://www.starcat.co.jp/ch/upload/channel/69/jsports1_logo.jpg",
+      name: "JSport 1"
     },
     {
-      name: "JSport_2-test",
+      id: "jsport_2_test",
       group: "BS2",
       url: "http://luong.utako.moe/js2/index.m3u8",
       logo: "https://www.starcat.co.jp/ch/upload/channel/70/jsports2_logo.jpg",
+      name: "JSport 2"
     },
     {
-      name: "JSport 3-test",
+      id: "jsport_3_test",
       group: "BS2",
       url: "http://luong.utako.moe/js3/index.m3u8",
       logo: "https://www.starcat.co.jp/ch/upload/channel/71/jsports3_logo.jpg",
+      name: "JSport 3"
     },
     {
-      name: "JSport 4-test",
+      id: "jsport_4_test",
       group: "BS2",
       url: "http://luong.utako.moe/js4/index.m3u8",
       logo: "https://www.starcat.co.jp/ch/upload/channel/74/jsports4_logo.jpg",
+      name: "JSport 4"
     },
     {
-      name: "AT-X",
-      group: "CS",
-      url: "http://vthanh.utako.moe/AT-X/index.m3u8",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/AT-X_logo.svg/2560px-AT-X_logo.svg.png",
-    },
-    {
-      name: "QVC",
-      group: "CS",
-      url: "https://cdn-live1.qvc.jp/iPhone/1501/1501.m3u8",
-      logo: "https://i.imgur.com/xWSzQ34.png",
-    },
-    {
-      name: "Space Shower TV-test",
+      id: "space_shower_tv_test",
       group: "CS2",
       url: "http://luong.utako.moe/spaceshower/index.m3u8",
       logo: "https://upload.wikimedia.org/wikipedia/commons/0/05/SPACE_SHOWER_TV.jpg",
-    },
-    {
-      name: "Pigoo-test (NSFW)",
-      group: "CS2",
-      url: "http://luong.utako.moe/pigoo/index.m3u8",
-      logo: "https://www.lyngsat.com/logo/tv/pp/pigoo-jp.png",
+      name: "Space Shower TV"
     }
   ];
